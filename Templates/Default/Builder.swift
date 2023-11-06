ec58ab07fc9cd63c71cbb961d0c85c937b8ebc54
Shu Kuro <shuhei.kuroda@mo-t.com>
2023-11-06 12:02:10 +0900

    テンプレートファイルをNeedleに適応した形に変更

--- Templates/Default/Builder.swift
+++ Templates/Default/Builder.swift
@@ -5,12 +5,13 @@
 //
 
 import RIBs
+import NeedleFoundation
 
-protocol ___VARIABLE_productName___Dependency: Dependency {
+protocol ___VARIABLE_productName___Dependency: NeedleFoundation.Dependency {
     var ___VARIABLE_productName_lowercased___ViewController: ___VARIABLE_productName___ViewControllable { get }
 }
 
-final class ___VARIABLE_productName___Component: Component<___VARIABLE_productName___Dependency> {
+final class ___VARIABLE_productName___Component: NeedleFoundation.Component<___VARIABLE_productName___Dependency> {
 
     fileprivate var ___VARIABLE_productName_lowercased___ViewController: ___VARIABLE_productName___ViewControllable {
         return dependency.___VARIABLE_productName_lowercased___ViewController
@@ -24,14 +25,13 @@ protocol ___VARIABLE_productName___Buildable: Buildable {
     func build(withListener listener: ___VARIABLE_productName___Listener) -> ___VARIABLE_productName___Routing
 }
 
-final class ___VARIABLE_productName___Builder: Builder<___VARIABLE_productName___Dependency>, ___VARIABLE_productName___Buildable {
-
-    override init(dependency: ___VARIABLE_productName___Dependency) {
-        super.init(dependency: dependency)
-    }
+final class ___VARIABLE_productName___Builder: ComponentizedBuilder<___VARIABLE_productName___Component, ___VARIABLE_productName___Routing, ___VARIABLE_productName___Listener, ()>, ___VARIABLE_productName___Buildable {
 
     func build(withListener listener: ___VARIABLE_productName___Listener) -> ___VARIABLE_productName___Routing {
-        let component = ___VARIABLE_productName___Component(dependency: dependency)
+        build(withDynamicBuildDependency: listener, dynamicComponentDependency: ())
+    }
+    
+    override func build(with component: ___VARIABLE_productName___Component, _ listener: ___VARIABLE_productName___Listener) -> ___VARIABLE_productName___Routing {
         let interactor = ___VARIABLE_productName___Interactor()
         interactor.listener = listener
         return ___VARIABLE_productName___Router(interactor: interactor,
--- Templates/OwnsView/Builder.swift
+++ Templates/OwnsView/Builder.swift
@@ -5,11 +5,12 @@
 //
 
 import RIBs
+import NeedleFoundation
 
-protocol ___VARIABLE_productName___Dependency: Dependency {
+protocol ___VARIABLE_productName___Dependency: NeedleFoundation.Dependency {
 }
 
-final class ___VARIABLE_productName___Component: Component<___VARIABLE_productName___Dependency> {
+final class ___VARIABLE_productName___Component: NeedleFoundation.Component<___VARIABLE_productName___Dependency> {
 }
 
 // MARK: - Builder
@@ -18,14 +19,13 @@ protocol ___VARIABLE_productName___Buildable: Buildable {
     func build(withListener listener: ___VARIABLE_productName___Listener) -> ___VARIABLE_productName___Routing
 }
 
-final class ___VARIABLE_productName___Builder: Builder<___VARIABLE_productName___Dependency>, ___VARIABLE_productName___Buildable {
-
-    override init(dependency: ___VARIABLE_productName___Dependency) {
-        super.init(dependency: dependency)
-    }
+final class ___VARIABLE_productName___Builder: ComponentizedBuilder<___VARIABLE_productName___Component, ___VARIABLE_productName___Routing, ___VARIABLE_productName___Listener, ()>, ___VARIABLE_productName___Buildable {
 
     func build(withListener listener: ___VARIABLE_productName___Listener) -> ___VARIABLE_productName___Routing {
-        let component = ___VARIABLE_productName___Component(dependency: dependency)
+        build(withDynamicBuildDependency: listener, dynamicComponentDependency: ())
+    }
+    
+    override func build(with component: ___VARIABLE_productName___Component, _ listener: ___VARIABLE_productName___Listener) -> ___VARIABLE_productName___Routing {
         let viewController = ___VARIABLE_productName___ViewController()
         let interactor = ___VARIABLE_productName___Interactor(presenter: viewController)
         interactor.listener = listener
