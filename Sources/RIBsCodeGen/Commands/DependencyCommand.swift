810edb999501b4b243adbfd30123e728d581f641
Shu Kuro <shuhei.kuroda@mo-t.com>
2023-11-09 10:26:48 +0900

    親Componentで子Componentを定義する際、引数があればfunctionにする

--- Sources/RIBsCodeGen/Commands/DependencyCommand.swift
+++ Sources/RIBsCodeGen/Commands/DependencyCommand.swift
@@ -280,8 +280,37 @@ private extension DependencyCommand {
 
         let dependencyInsertIndex = text.utf8.index(text.startIndex, offsetBy: insertPosition)
         
-        text.insert(contentsOf: "var \(child.lowercasedFirstLetter())Component: \(child)Component {\n\(child)Component(parent: self)\n}\n", at: dependencyInsertIndex)
+        // 子Componentがinitを持っているか（かつ引数を取得できるか）どうかを判定する
+        let childBuilderFile = File(path: childBuilderPath)!
+        let childBuilderFileStructure = try Structure(file: childBuilderFile)
+        let childBuilderClasses = childBuilderFileStructure.dictionary.getSubStructures().filterByKeyKind(.class)
+        
+        guard let childComponentClass = childBuilderClasses.filterByKeyName("\(child)Component").first else {
+            print("  Not found \(child)Component class.".red.bold)
+            throw Error.failedToAddChildBuilder
+        }
+
+        let initArguments = getChildComponentInitArguments(childComponentClass: childComponentClass)
+        if initArguments.isEmpty {
+            text.insert(contentsOf: "var \(child.lowercasedFirstLetter())Component: \(child)Component {\n\(child)Component(parent: self)\n}\n", at: dependencyInsertIndex)
+        } else {
+            let arguments = initArguments.map { "\($0.name): \($0.type)" }.first!
+            let innerArguments = initArguments.map { $0.name }.map { "\($0): \($0)" }.joined(separator: ", ")
+            text.insert(contentsOf: "func \(child.lowercasedFirstLetter())Component(\(arguments)) -> \(child)Component {\n\(child)Component(parent: self, \(innerArguments))\n}\n", at: dependencyInsertIndex)
+        }
+
         write(text: text, toPath: parentBuilderPath)
+    }
+
+    private func getChildComponentInitArguments(childComponentClass: [String: SourceKitRepresentable]) -> [(name: String, type: String)] {
+        let initStructure = childComponentClass.getSubStructures().extractDictionaryContainsKeyName("init")
+        let attributes = initStructure.getAttributes()
+        let initArguments = initStructure.getSubStructures().filterByKeyKind(.varParameter)
+        let childComponentArguments = initArguments
+            .filter { $0.getKeyName() != "parent" }
+            .map { (name: $0.getKeyName(), type: $0.getTypeName()) }
+        
+        return childComponentArguments
     }
 
     func addChildBuilderInitialize(parentBuilderPath: String) throws {
